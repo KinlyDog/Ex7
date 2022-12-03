a4b40a344a70612556e58f88d230c311124a3ad0
Andrey Otryaskin <kinlydog@gmail.com>
2022-12-04 01:16:29 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -1,17 +1,17 @@
 public class Level1 {
     public static int[] UFO(int n, int[] data, boolean octal) {
-        int[] ma = new int[n];
+        int[] dataDecoderToDec = new int[n];
 
         for (int i = 0; i < n; i++) {
             String str = String.valueOf(data[i]);
 
             if (octal) {
-                ma[i] = Integer.parseInt(str, 8);
+                dataDecoderToDec[i] = Integer.parseInt(str, 8);
             } else {
-                ma[i] = Integer.parseInt(str, 16);
+                dataDecoderToDec[i] = Integer.parseInt(str, 16);
             }
         }
 
-        return ma;
+        return dataDecoderToDec;
     }
 }
